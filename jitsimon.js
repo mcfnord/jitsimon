e50a3531d294ecf5f2da0e077e054329dc3a3eaf
jrd <mcfnord@gmail.com>
2021-11-28 11:02:22 -0800

    spit JSON

--- jitsimon.js
+++ jitsimon.js
@@ -2,15 +2,18 @@ const puppeteer = require("puppeteer");
 
 (async () => {
 
-jamjits = '[{"jam_ip" : "35.0.0.1:454545", "jit_url" : "https://meet.jit.si/CKsJamulusMucMusic#userInfo.displayName=\"jamulus.live\"}]' ;
+jamjits = '[{"jam_ip" : "35.0.0.1:454545", "jit_url" : "https://meet.jit.si/CKsJamulusMucMusic"}, \
+{"jam_ip" : "35.0.0.1:454545", "jit_url" : "https://meet.jit.si/CKsJamulusMucMusic"} \
+]' ;
 var jjs = JSON.parse(jamjits);
+var probed_calls = [] ;
 for (const jj of jjs) {
   const browser = await puppeteer.launch({
     args: [ '--use-fake-ui-for-media-stream' ]
     // , executablePath: '/usr/bin/chromium-browser'
   })
   const page = await browser.newPage();
-  await page.goto("https://meet.jit.si/CKsJamulusMucMusic#userInfo.displayName=\"jamulus.live\"");
+  await page.goto(jj.jit_url);
   await page.waitForTimeout(3000);
   await page.click('.action-btn');
   await page.waitForTimeout(1000);
@@ -19,11 +22,13 @@ for (const jj of jjs) {
   await element.click();
   await page.waitForTimeout(1000);
   let targetzone = await page.$$('.videocontainer') ;
-  let num_here = targetzone.length;
+  let num_here = targetzone.length - 2;
   await page.waitForTimeout(1000);
   await page.click('.hangup-button');
-  await page.waitForTimeout(10000);
+  await page.waitForTimeout(5000);
   browser.close();
-  console.log(jj.jan_ip + jj.jit_url + num_here);
+  probed_calls.push( { "ip" : jj.jam_ip, "url" : jj.jit_url, "actives" : num_here } );
+}
+console.log(probed_calls);
 })();
 
