8f9b6f655a61021b45a711d545c6b1c842eef361
jbird <git.x.jrd@xoxy.net>
2021-11-28 18:01:04 +0000

    pseudo

--- jitsimon.js
+++ jitsimon.js
@@ -1,10 +1,14 @@
 const puppeteer = require("puppeteer");
 
 (async () => {
-	const browser = await puppeteer.launch({
-          args: [ '--use-fake-ui-for-media-stream' ]
-//          , executablePath: '/usr/bin/chromium-browser'
-	})
+
+jamjits = '[{"jam_ip" : "35.0.0.1:454545", "jit_url" : "https://meet.jit.si/CKsJamulusMucMusic#userInfo.displayName=\"jamulus.live\"}]' ;
+var jjs = JSON.parse(jamjits);
+for (const jj of jjs) {
+  const browser = await puppeteer.launch({
+    args: [ '--use-fake-ui-for-media-stream' ]
+    // , executablePath: '/usr/bin/chromium-browser'
+  })
   const page = await browser.newPage();
   await page.goto("https://meet.jit.si/CKsJamulusMucMusic#userInfo.displayName=\"jamulus.live\"");
   await page.waitForTimeout(3000);
@@ -14,14 +18,12 @@ const puppeteer = require("puppeteer");
   await page.waitForTimeout(2000);
   await element.click();
   await page.waitForTimeout(1000);
-
   let targetzone = await page.$$('.videocontainer') ;
-//console.log(targetzone);
-  let subcount = targetzone.length;
-  console.log(subcount);
+  let num_here = targetzone.length;
   await page.waitForTimeout(1000);
   await page.click('.hangup-button');
   await page.waitForTimeout(10000);
   browser.close();
+  console.log(jj.jan_ip + jj.jit_url + num_here);
 })();
 
