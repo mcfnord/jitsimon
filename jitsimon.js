a9d961e7cdb4719e42c9578c639f0724b62e48dc
jrd <mcfnord@gmail.com>
2021-12-04 14:13:13 -0800

    ugh

--- jitsimon.js
+++ jitsimon.js
@@ -1,21 +1,47 @@
 const puppeteer = require("puppeteer");
 
+// const fs = require ('fs');
+
 (async () => {
 
-jamjits = '[{"jam_ip" : "' + process.argv[2] + '", "jit_url" : "' + process.argv[3] + '"}]' ;
-var jjs = JSON.parse(jamjits);
+  // feed me a local file. i'll parse it and produce output that jamfan can ingest.
+  // jjs = [];
+
+  var jjs = JSON.parse(require('fs').readFileSync(process.argv[2], 'utf8'));
+
+  /*
+  fs.readFile(process.argv[2], 'utf8', (error, data) => {
+    if(error){
+       console.log(error);
+       return;
+    }
+    jjs = JSON.parse(data);
+    console.log(jjs);
+})
+*/
+
+// jamjits =
+//  '[{"jam_ip" : "' + process.argv[2] + '", "jit_url" : "' + process.argv[3] + '"}]' ;
+console.log("hahaha");
+console.log(jjs) ;
 var probed_calls = [] ;
 for (const jj of jjs) {
+  console.log("sup " + JSON.stringify(jj));
   const browser = await puppeteer.launch({
-    executablePath: "/usr/bin/chromium-browser",
+//    executablePath: "/usr/bin/chromium-browser",
     args: [ "--use-fake-ui-for-media-stream" ]
   });
   const page = await browser.newPage();
   await page.goto(jj.jit_url);
   await page.waitForTimeout(3000);
-  await page.click('.action-btn');
+
+  //await page.click('.action-btn');
+  var joinElement = await page.$('[aria-label="Join meeting"]');
+  page.click(joinElement);
+  console.log("Clicked 1.");
   await page.waitForTimeout(1000);
   var element = await page.$('[aria-label="Toggle tile view"]');
+  console.log("Clicked 2.");
   await page.waitForTimeout(2000);
   await element.click();
   await page.waitForTimeout(1000);
