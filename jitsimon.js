12745aec36702b59278d4d4892e6d1bf1bff341f
jrd <jamfan.x.jrd@xoxy.net>
2021-12-05 02:16:16 +0000

    well maybe this works

--- jitsimon.js
+++ jitsimon.js
@@ -1,60 +1,55 @@
 const puppeteer = require("puppeteer");
 
+const { promisify } = require('util');
+const sleep = promisify(setTimeout);
+
 // const fs = require ('fs');
 
 (async () => {
-
-  // feed me a local file. i'll parse it and produce output that jamfan can ingest.
-  // jjs = [];
-
   var jjs = JSON.parse(require('fs').readFileSync(process.argv[2], 'utf8'));
 
-  /*
-  fs.readFile(process.argv[2], 'utf8', (error, data) => {
-    if(error){
-       console.log(error);
-       return;
-    }
-    jjs = JSON.parse(data);
-    console.log(jjs);
-})
-*/
-
-// jamjits =
-//  '[{"jam_ip" : "' + process.argv[2] + '", "jit_url" : "' + process.argv[3] + '"}]' ;
-console.log("hahaha");
-console.log(jjs) ;
 var probed_calls = [] ;
-for (const jj of jjs) {
-  console.log("sup " + JSON.stringify(jj));
-  const browser = await puppeteer.launch({
-//    executablePath: "/usr/bin/chromium-browser",
-    args: [ "--use-fake-ui-for-media-stream" ]
+const browser = await puppeteer.launch({
+    executablePath: "/snap/bin/chromium",
+    args: [ "--use-fake-ui-for-media-stream", "--no-sandbox" ]
   });
-  const page = await browser.newPage();
-  await page.goto(jj.jit_url);
-  await page.waitForTimeout(3000);
 
-  //await page.click('.action-btn');
-  var joinElement = await page.$('[aria-label="Join meeting"]');
-  page.click(joinElement);
-  console.log("Clicked 1.");
-  await page.waitForTimeout(1000);
-  console.log("Clicked 1.5");
-  var page2 = browser.pages()[0];
-  var element = await page2.$('[aria-label="Toggle tile view"]');
-  console.log("Clicked 2.");
-  await page2.waitForTimeout(2000);
-  await element.click();
-  await page2.waitForTimeout(1000);
-  let targetzone = await page2.$$('.videocontainer') ;
+await sleep(5000) ;
+
+for (const jj of jjs) {
+  const page = await browser.newPage();
+  await sleep(3000);
+  await page.goto(jj.jit_url);
+  await sleep(3000);
+  await page.click('[aria-label="Join meeting"]');
+  await sleep(3000);
+  await page.click('[aria-label="Toggle tile view"]') ;
+//  var element = await page2.$('[aria-label="Toggle tile view"]');
+  await sleep(2000);
+  let targetzone = await page.$$('.videocontainer') ;
   let num_here = targetzone.length - 2;
-  await page2.waitForTimeout(1000);
-  await page2.click('.hangup-button');
-  await page2.waitForTimeout(5000);
-  browser.close();
-  probed_calls.push( { "ip" : jj.jam_ip, "url" : jj.jit_url, "actives" : num_here } );
+  await sleep(1000) ;
+  await page.click('.hangup-button');
+  await sleep(1000);
+//  browser.close();
+  console.log("for " + jj.jam_ip + " jitsi " + jj.jit_url + " has " + num_here + " connections.");
+  if(num_here > 0)
+    probed_calls.push( { "ip" : jj.jam_ip, "url" : jj.jit_url, "actives" : num_here } );
 }
-console.log(probed_calls);
+browser.close();
+
+var output = "" ;
+for(const item of probed_calls)
+{
+    output = output + item.ip + " " + item.url + "\n" ;
+}
+
+console.log(output);
+
+const FileSystem = require("fs");
+FileSystem.writeFile(process.argv[3], output, (error) => {
+    if (error) throw error;
+  });
+
 })();
 
